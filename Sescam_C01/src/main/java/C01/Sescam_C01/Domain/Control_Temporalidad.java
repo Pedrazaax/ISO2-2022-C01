f98449bc6670a41122a6e7ea5e515248db3afc7a
Carlos Pedraza Antona <74327838+Pedrazaax@users.noreply.github.com>
2021-12-14 20:11:54 +0100

    Añadido código v2.0.0
    CdU 4, Iteración 5.

---
+++ Sescam_C01/src/main/java/C01/Sescam_C01/Domain/Control_Temporalidad.java
@@ -0,0 +1,10 @@
+package C01.Sescam_C01.Domain;
+
+public class Control_Temporalidad {
+
+	public void viewTemporalidad() {
+		// TODO - implement Control_Temporalidad.viewTemporalidad
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Sescam_C01/src/main/java/C01/Sescam_C01/Presentation/IU_GestorTemporalidad.java
@@ -0,0 +1,10 @@
+package C01.Sescam_C01.Presentation;
+
+public class IU_GestorTemporalidad {
+
+	public void submitTemporalidad() {
+		// TODO - implement IU_GestorTemporalidad.submitTemporalidad
+		throw new UnsupportedOperationException();
+	}
+
+}
